86097ce24576441fe87dfaa5486a3e7b343813a4
Daniel Mendes <dmendesrbh@gmail.com>
2024-03-13 17:25:55 -0300

    Controller1

---
+++ ConnectLocal/Controllers/ContratantesController.cs
@@ -0,0 +1,26 @@
+﻿using ConnectLocal.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConnectLocal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContratantesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ContratantesController(AppDbContext context)
+        {
+            _context = context;         
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var model = await _context.Contratantes.ToListAsync();
+            return Ok(model);
+        }
+    }
+}
